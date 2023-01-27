427ca52c66583877911df56613a9d337f6c853bb
Mahmoud Hosni <mah1998mod@gmail.com>
2023-01-27 18:19:55 +0200

    added hover menu

---
+++ src/app/hover-animation-menu/hover-animation-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hover-animation-menu',
+  templateUrl: './hover-animation-menu.component.html',
+  styleUrls: ['./hover-animation-menu.component.css']
+})
+export class HoverAnimationMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
